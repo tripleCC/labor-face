3c3c23f0f9ccff94b4ac9c75ae3ca20fe3d905d9
tripleCC <triplec.linux@gmail.com>
2018-12-04 13:21:32 +0800

    添加是否允许发送钉钉消息 http://git.2dfire-inc.com/ios/labor/issues/39

--- src/features/deploys/AddMainDeployModal.js
+++ src/features/deploys/AddMainDeployModal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Form, Input, message } from 'antd';
+import { Modal, Form, Input, Checkbox } from 'antd';
 
 const FormItem = Form.Item;
 
@@ -25,6 +25,12 @@ class AddMainDeployModal extends React.PureComponent {
     const formItemLayout = {
       labelCol: { span: 5 },
       wrapperCol: { span: 15 },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        span: 15,
+        offset: 5,
+      },
     };
     return (
       <Modal
@@ -70,6 +76,12 @@ class AddMainDeployModal extends React.PureComponent {
             rules: [{ required: true, message: '请输入分支名称！' }],
             // initialValue: 'release/',
           })(<Input placeholder="请输入" />)}
+        </FormItem>
+        <FormItem {...tailFormItemLayout}>
+          {getFieldDecorator('should_push_ding', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>是否允许发送钉钉消息</Checkbox>)}
         </FormItem>
       </Modal>
     );
